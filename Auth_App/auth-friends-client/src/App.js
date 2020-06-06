649713cca82b8ce9292074d2b8891e856f88e28e
Michael Gregory <michael.gregory8584@gmail.com>
2020-06-06 00:47:31 -0500

    New dependencies addeed added: react-router-dom
    Finished App routes I think.
    LoginForm built.
    Goint to work on Friends component.

--- Auth_App/auth-friends-client/src/App.js
+++ Auth_App/auth-friends-client/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Friends from './components/Friends'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+        </nav>
+        <Navigation />
+        <Switch>
+          <ProtectedRoute path="/friends" component={ Friends } />
+          <Route path="/login" component={ LoginForm } />
+          <Route component={ LoginForm } />
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
